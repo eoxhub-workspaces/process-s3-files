a4b0f4f7b3e36d44af53d15c84021c74662ff997
Josef Prenner <josef.prenner@eox.at>
2024-11-29 14:47:36 +0100

    add results json upload

--- README.md
+++ README.md
@@ -4,8 +4,9 @@ Creates a json `{"urls": ["url-1", "url-2", "url-n"]}` and sets the permission t
 For use with argo workflows to parse generated files. The script processes `NUM_FILES` sorted by last modified, sets the acl and stores the url in the json.
 
 
-The json will be printed to stdout and stored in a file at `/tmp/out.json`.
+The json will be printed to stdout and stored in a file at `/tmp/out.json`.  
+If `PUBLISH_JSON` is set to `true` and the `PUBLISH_JSON_*` env vars are configured the result json will be uploaded to the base bucket path with the generated filename. See `docker-compose.yaml` for details.
 
 # Environment
 
-All environment variables listed in `docker-compose.yaml` are mandatory.
+All environment variables listed in `docker-compose.yaml` are mandatory, except for `PUBLISH_JSON_*` if `PUBLISH_JSON=false`.
--- process_s3_files.sh
+++ process_s3_files.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
+if [ "$DEBUG" == "true" ]; then
+    env
+    DEBUGFLAG="--debug"
+fi
 # Get last modified file in $BUCKET at $BPATH
-FILES=$(aws s3 ls --region $AWS_REGION --endpoint-url $AWS_S3_ENDPOINT s3://$BUCKET/$BPATH/ | sort | tail -n $NUM_FILES | awk '{ print $4 }')
+FILES=$(aws $DEBUGFLAG s3 ls --region $AWS_REGION --endpoint-url $AWS_S3_ENDPOINT s3://$BUCKET/$BPATH/ | sort | tail -n $NUM_FILES | awk '{ print $4 }')
 URL_LIST="[]"
 while IFS= read -r FILE; do
     echo Processing $FILE
     # set acl to $ACL
-    aws s3api put-object-acl --region $AWS_REGION --endpoint-url $AWS_S3_ENDPOINT --bucket $BUCKET --key $BPATH/$FILE --acl $ACL
+    aws $DEBUGFLAG s3api put-object-acl --region $AWS_REGION --endpoint-url $AWS_S3_ENDPOINT --bucket $BUCKET --key $BPATH/$FILE --acl $ACL
     echo Permissions set to $ACL
     # add to url array
     URL=$AWS_S3_ENDPOINT/$BUCKET/$BPATH/$FILE
     URL_LIST=$(echo $URL_LIST | jq -r --arg URL "$URL" '. += [$URL]')
 done <<< $FILES
+
 # write file url to out.json
 echo "{\"urls\":$URL_LIST}" > /tmp/out.json
+# store json in bucket
+if [ "$PUBLISH_JSON" == "true" ]; then
+    declare -n PUBLISH_JSON_FILENAME_VALUE=${PUBLISH_JSON_FILENAME_ENV_VAR}
+    if [[ $PUBLISH_JSON_FILENAME_VALUE =~ $PUBLISH_JSON_FILENAME_REGEX ]]; then
+        JSON_FILENAME_BASE=${BASH_REMATCH[$PUBLISH_JSON_FILENAME_REGEX_MATCH_GROUP]}
+        JSON_FILEPATH=$AWS_S3_ENDPOINT/$BUCKET/$JSON_FILENAME_BASE.json
+        aws $DEBUGFLAG s3 cp /tmp/out.json --region $AWS_REGION --endpoint-url $AWS_S3_ENDPOINT s3://$BUCKET/$JSON_FILENAME_BASE.json --acl $PUBLISH_JSON_ACL
+    else
+        echo Regex did not match for $PUBLISH_JSON_FILENAME_ENV_VAR - $PUBLISH_JSON_FILENAME_VALUE
+    fi
+fi
 cat /tmp/out.json
