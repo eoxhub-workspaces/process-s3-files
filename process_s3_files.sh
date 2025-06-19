508a476336fd71683e031008a57ef22c66ab4b5b
Lubomir Dolezal <lubomir.dolezal@eox.at>
2025-06-19 19:36:08 +0200

    add --group argument parsing and keep legacy mode

--- process_s3_files.sh
+++ process_s3_files.sh
@@ -3,6 +3,30 @@ if [ "$DEBUG" == "true" ]; then
     env
     DEBUGFLAG="--debug"
 fi
+
+GROUP_ARGS=()
+# parse GROUP_ARGS arguments
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    --group)
+      if [[ -n "${2-}" ]]; then
+        GROUP_ARGS+=("$2")
+        shift 2
+      else
+        echo "Error: --group requires an argument."
+        exit 1
+      fi
+      ;;
+    --group=*)
+      GROUP_ARGS+=("${1#*=}")
+      shift
+      ;;
+    *)
+      echo "Unknown option: $1"
+      exit 1
+      ;;
+  esac
+done
 # Get last modified file in $BUCKET at $BPATH
 FILES=$(aws $DEBUGFLAG s3 ls --region $AWS_REGION --endpoint-url $AWS_S3_ENDPOINT s3://$BUCKET/$BPATH/ | sort | tail -n $NUM_FILES | awk '{ print $4 }')
 URL_LIST="[]"
@@ -13,11 +37,17 @@ while IFS= read -r FILE; do
     echo Permissions set to $ACL
     # add to url array
     URL=$AWS_S3_ENDPOINT/$BUCKET/$BPATH/$FILE
-    URL_LIST=$(echo $URL_LIST | jq -r --arg URL "$URL" '. += [$URL]')
+    # If no group provided, create URL array (legacy mode)
+    if [[ ${#GROUP_ARGS[@]} -eq 0 ]]; then
+        echo "No --group parameter supplied, will create URLs array."
+        URL_LIST=$(echo $URL_LIST | jq -r --arg URL "$URL" '. += [$URL]')
+    fi
 done <<< $FILES
 
 # write file url to out.json
-echo "{\"urls\":$URL_LIST}" > /tmp/out.json
+if [[ ${#GROUP_ARGS[@]} -eq 0 ]]; then
+    echo "{\"urls\":$URL_LIST}" > /tmp/out.json
+fi
 # store json in bucket
 if [ "$PUBLISH_JSON" == "true" ]; then
     declare -n PUBLISH_JSON_FILENAME_VALUE=${PUBLISH_JSON_FILENAME_ENV_VAR}
